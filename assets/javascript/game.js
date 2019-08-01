d69821e50bc03960ce60b078bef723a0fe2ae266
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-01 17:31:21 -0500

    added some psuedocode, comments, global variables and basic logic/control structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,36 @@
-    
-    const words = ['pineapple', 'squash', 'astronomy', 'rattlesnake', 'xylophone', 'watermelon', 'bumblebee']
-    
-    var directionsText = document.getElementById("directions-text");
 
-    document.onkeyup = function(event) {
-        const control = event.key.toLowerCase;
+const words = ['pineapple', 'squash', 'astronomy', 'rattlesnake', 'xylophone', 'watermelon', 'bumblebee'];
+
+let isPlaying = false;      //control variable
+let incompleteWord = "";    //stores the in-progress word with correct letters filled in
+let solution = "";          //actual solution
+
+
+const directionsText = document.getElementById("directions-text");
+
+document.onkeyup = function (event) {
+    const control = event.key.toLowerCase;
+
+    //(re)initialization
+    if (!isPlaying) {
+        // display instructions
+        
+
+
         // select a word to be guessed
-        // populate new array/string to store 
-        // display black spaces on screen,
+        answer = words[Math.floor(Math.random() * words.length)]
 
+        isPlaying = true
 
     }
+    else{
+        
+
+        // populate new array/string to store 
+        // display blank spaces on screen
+        // 
+        // if player wins, print win text and print text asking them if they want to play again
+
+        if()
+    }
+}
