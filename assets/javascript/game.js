bbd051e18f6852b230e42aa43dd122bf4bba04a9
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-01 16:56:30 -0500

    added basic html and javascript structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+    
+    const words = ['pineapple', 'squash', 'astronomy', 'rattlesnake', 'xylophone', 'watermelon', 'bumblebee']
+    
+    var directionsText = document.getElementById("directions-text");
+
+    document.onkeyup = function(event) {
+        const control = event.key.toLowerCase;
+        // select a word to be guessed
+        // populate new array/string to store 
+        // display black spaces on screen,
+
+
+    }
