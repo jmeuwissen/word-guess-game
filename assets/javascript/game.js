c96e3b93a3e028f6b0ee013ea3bd3004347fb0f8
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-01 19:11:52 -0500

    fleshed out control structure, psuedocode nearly complete. implemented logic for: intially populating incompleteword, filling incompleteword with correct guesses, adding incorrect char guesses to incorrect chars. I know its a lot but i was on a roll. probably bad practice.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,81 @@
 
-const words = ['pineapple', 'squash', 'astronomy', 'rattlesnake', 'xylophone', 'watermelon', 'bumblebee'];
+const WORDS = ["pineapple", "squash", "astronomy", "rattlesnake", "xylophone", "watermelon", "bumblebee", "butterscotch", "headphones", "obfuscation", "iufuegweew"]; //muahahaha
 
 let isPlaying = false;      //control variable
 let incompleteWord = "";    //stores the in-progress word with correct letters filled in
-let solution = "";          //actual solution
+let answer = "";            //actual answer
+let incorrectChars = "";    //string of incorrect guesses
+
+const MAX_INCORRECT = 7;
 
 
 const directionsText = document.getElementById("directions-text");
 
 document.onkeyup = function (event) {
-    const control = event.key.toLowerCase;
+    const guess = event.key.toLowerCase;
 
     //(re)initialization
     if (!isPlaying) {
-        // display instructions
+        // display instructions in user feedback element
+
+
+        // (re)initialize in-progress word, incorrect chars
         
 
+        // select a random word to be guessed
+        answer = WORDS[Math.floor(Math.random() * WORDS.length)]
 
-        // select a word to be guessed
-        answer = words[Math.floor(Math.random() * words.length)]
+        // populate incomplete word with blank spaces (underscores)
+        for (let i = 0; i < answer.length; i++) {
+            incompleteWord = incompleteWord + "_";
+        }
 
-        isPlaying = true
+        //changes control variable to start gameplay
+        isPlaying = true;
 
     }
-    else{
-        
 
-        // populate new array/string to store 
-        // display blank spaces on screen
-        // 
-        // if player wins, print win text and print text asking them if they want to play again
 
-        if()
+    // main gameplay logic
+    else {
+
+
+
+        // display incomplete word on screen
+        // display incorrect characters
+        // display number of guesses remaining
+
+
+        // check if character has already been guessed and is an incorrect guess
+        if (incorrectChars.indexOf(guess) !== -1) {
+            //display "you already guessed that!" in user feedback element
+        }
+
+        // check if guessed letter is in answer
+        else if (answer.indexOf(guess) !== -1) {
+            //display "correct guess!" in user feedback element
+
+            //replace underscores with guess character in indexes that correspond to correct ones in answer
+            //iterates through characters of string
+            for (let i = 0; i < answer.length; i++) {
+                //replaces underscore with if guess character is at the current index
+                if (answer[i] === guess) {
+                    incompleteWord[i] = answer[i];
+                }
+            }
+
+        }
+        else {
+            //display "incorrect guess" in user feedback element
+            //put guess in incorrect char array
+            incorrectChars = incorrectChars + guess;
+        }
+
+
+        // check if the current player wordprogress is the solution
+        if (incompleteWord === solution) {
+            //display win text and display text asking them if they want to play again in user feedback element
+
+        }
     }
 }
