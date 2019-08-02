1ad856b9a0d37896b7bfdae91cf23440c44e744f
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-01 19:54:57 -0500

    added win and lose conditions their global counter variables, started some display updating logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,24 +5,38 @@ let isPlaying = false;      //control variable
 let incompleteWord = "";    //stores the in-progress word with correct letters filled in
 let answer = "";            //actual answer
 let incorrectChars = "";    //string of incorrect guesses
+let wins = 0;               //win counter
+let losses = 0;             //loss counter
 
 const MAX_INCORRECT = 7;
 
 
 const directionsText = document.getElementById("directions-text");
 
+
+//display metagame data
+
+//wins
+//losses
+
 document.onkeyup = function (event) {
     const guess = event.key.toLowerCase;
+
+
+    //I should have proabably done this whole control flow with a switch instead of nested if else
+    //time and stamina permitting, I will
 
     //(re)initialization
     if (!isPlaying) {
         // display instructions in user feedback element
 
 
-        // (re)initialize in-progress word, incorrect chars
+        // (re)initialize incorrect chars
+        incorrectChars = "";
+        
         
 
-        // select a random word to be guessed
+        // select a random word to be guessed 
         answer = WORDS[Math.floor(Math.random() * WORDS.length)]
 
         // populate incomplete word with blank spaces (underscores)
@@ -30,7 +44,7 @@ document.onkeyup = function (event) {
             incompleteWord = incompleteWord + "_";
         }
 
-        //changes control variable to start gameplay
+        //changes control variable to start gameplay 
         isPlaying = true;
 
     }
@@ -41,14 +55,18 @@ document.onkeyup = function (event) {
 
 
 
-        // display incomplete word on screen
-        // display incorrect characters
-        // display number of guesses remaining
+        /// display incomplete word on screen with a space between each character
+        for(let i = 0; i < answer.length; i++){
+
+        }
+        /// display incorrect characters
+        /// display number of guesses remaining
+        MAX_INCORRECT - incorrectChars.length;
 
 
         // check if character has already been guessed and is an incorrect guess
         if (incorrectChars.indexOf(guess) !== -1) {
-            //display "you already guessed that!" in user feedback element
+            ///display "you already guessed that!" in user feedback element
         }
 
         // check if guessed letter is in answer
@@ -56,13 +74,13 @@ document.onkeyup = function (event) {
             //display "correct guess!" in user feedback element
 
             //replace underscores with guess character in indexes that correspond to correct ones in answer
-            //iterates through characters of string
-            for (let i = 0; i < answer.length; i++) {
+            for (let i = 0; i < answer.length; i++) { //iterates through characters of string
                 //replaces underscore with if guess character is at the current index
                 if (answer[i] === guess) {
                     incompleteWord[i] = answer[i];
                 }
             }
+            //update 
 
         }
         else {
@@ -70,11 +88,28 @@ document.onkeyup = function (event) {
             //put guess in incorrect char array
             incorrectChars = incorrectChars + guess;
         }
+        
+        //lose condition
+        if(MAX_INCORRECT - incorrectChars.length < 1){
+            //display loss text and display text asking them if they want to play again in user feedback element
 
 
-        // check if the current player wordprogress is the solution
+            //increment losses
+            losses++;
+
+            //set control variable to reinitialize
+            isPlaying = false;
+        }
+
+        // win condition
         if (incompleteWord === solution) {
             //display win text and display text asking them if they want to play again in user feedback element
+
+            wins++;
+            ///update win display
+
+            //set control variable to reinitialize
+            isPlaying = false;
 
         }
     }
