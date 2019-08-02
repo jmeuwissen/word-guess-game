4fe377f2b2cd349867eafd2d8a36435dd4f1c7eb
Jack Meuwissen <jmeuwissen42@gmail.com>
2019-08-01 23:41:36 -0500

    it works!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,84 @@
 const WORDS = ["pineapple", "squash", "astronomy", "rattlesnake", "xylophone", "watermelon", "bumblebee", "butterscotch", "headphones", "obfuscation", "iufuegweew"]; //muahahaha
 
 let isPlaying = false;      //control variable
-let incompleteWord = "";    //stores the in-progress word with correct letters filled in
+let incompleteWord = [];    //stores the in-progress word as an array of strings //hooray for immutable strings... javascript is fun
 let answer = "";            //actual answer
-let incorrectChars = "";    //string of incorrect guesses
+let wrongChars = "";        //string of incorrect guesses
 let wins = 0;               //win counter
 let losses = 0;             //loss counter
+let remainingGuesses = 0;
 
-const MAX_INCORRECT = 7;
 
-
-const directionsText = document.getElementById("directions-text");
+const directionsTextEL = document.getElementById("directions-text");
+const winsEL = document.getElementById("wins");
+const lossesEL = document.getElementById("losses");
+const wrongCharsEL = document.getElementById("wrong-chars");
+const remainingGuessesEL = document.getElementById("guesses-remaining");
+const incompleteWordEL = document.getElementById("incomplete-word");
+const messageEL = document.getElementById("message");
 
 
 //display metagame data
+
+//incompleteWord toString function
+function arrayToString(arr){
+    let temp = "";
+    for(let i = 0; i < arr.length; i++){
+        temp = temp  + arr[i] + " ";
+    }
+    return temp;
+}
 
 //wins
 //losses
 
 document.onkeyup = function (event) {
-    const guess = event.key.toLowerCase;
+    
+    const guess = event.key.toLowerCase();  
 
 
     //I should have proabably done this whole control flow with a switch instead of nested if else
     //time and stamina permitting, I will
 
-    //(re)initialization
+    /////////////////(re)initialization/////////////////
     if (!isPlaying) {
-        // display instructions in user feedback element
-
+        directionsTextEL.textContent = " ";
 
         // (re)initialize incorrect chars
-        incorrectChars = "";
+        wrongChars = "";
+        incompleteWord = [];
+        
+        
+        messageEL.textContent = "Here's a round of Word Guess!"; 
+        winsEL.textContent = "Wins: " + wins;
+        lossesEL.textContent = "Losses: " + losses;
+        wrongCharsEL.textContent = "Incorrect characters: " + wrongChars;
+        remainingGuessesEL.textContent = "Guesses remaining: " + remainingGuesses;
+        
+
+
+        
+        console.log(wrongChars.length);
+
+        remainingGuesses = 7;
         
         
 
         // select a random word to be guessed 
         answer = WORDS[Math.floor(Math.random() * WORDS.length)]
+        console.log("answer: " + answer);
 
         // populate incomplete word with blank spaces (underscores)
+
         for (let i = 0; i < answer.length; i++) {
-            incompleteWord = incompleteWord + "_";
+            incompleteWord.push("_");
+            incompleteWord.t
+
+
         }
+        incompleteWordEL.textContent = "Incomplete word: " + arrayToString(incompleteWord);
+        console.log("incompleteWord: " + incompleteWord);
+
 
         //changes control variable to start gameplay 
         isPlaying = true;
@@ -50,48 +87,91 @@ document.onkeyup = function (event) {
     }
 
 
-    // main gameplay logic
-    else {
 
+
+
+
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////// MAIN GAMEPLAY LOGIC /////////////////////////
+    else {
+        
+        console.log("entered main loop")
+
+        // check if character has already been guessed and is an incorrect guess
+        if (wrongChars.indexOf(guess) !== -1 || incompleteWord.indexOf(guess) !== -1) {
+            ///display "you already guessed that!" in user feedback element
+            messageEL.textContent = "Pick a letter that you haven't guessed, please...";
+        }
+
+
+
+
+
+        /////////////////////RIGHT GUESS//////////////////////
+
+        else if (answer.indexOf(guess) !== -1) {
+            console.log("nice");
+            //display "correct guess!" in user feedback element
+            messageEL.textContent = "You got it!";
+
+            //replace underscores with guess character in indexes that correspond to correct ones in answer
+            for (let i = 0; i < answer.length; i++) { 
+                
+                //replaces underscore with if guess character is at the current index
+                if (answer[i] === guess) {
+                    
+
+                    incompleteWord[i] = guess;
+                    console.log(true + " " + incompleteWord[i]);
+                }
+            }
+            
+            //update incompleteWord
+            incompleteWordEL.textContent = "Incomplete word: " + arrayToString(incompleteWord);
+
+            
+        }
+
+
+        /////////////////////WRONG GUESS//////////////////////
+
+        else {
+            console.log("oops");
+            console.log("guess char: "+ guess);
+
+            //display "incorrect guess" in user feedback element
+            messageEL.textContent = "That ain't right";
+
+            //put guess in incorrect char array
+            wrongChars = wrongChars + guess;
+            //update wrong-chars display
+            wrongCharsEL.textContent = "Incorrect characters: " + wrongChars;
+
+            //update remaining guesses
+            remainingGuesses--;
+            remainingGuessesEL.textContent = "Guesses remaining: " + remainingGuesses;
+
+        }
 
 
         /// display incomplete word on screen with a space between each character
         for(let i = 0; i < answer.length; i++){
 
         }
-        /// display incorrect characters
-        /// display number of guesses remaining
-        MAX_INCORRECT - incorrectChars.length;
 
 
-        // check if character has already been guessed and is an incorrect guess
-        if (incorrectChars.indexOf(guess) !== -1) {
-            ///display "you already guessed that!" in user feedback element
-        }
+        /////////////////////GAME OVER////////////////////
 
-        // check if guessed letter is in answer
-        else if (answer.indexOf(guess) !== -1) {
-            //display "correct guess!" in user feedback element
-
-            //replace underscores with guess character in indexes that correspond to correct ones in answer
-            for (let i = 0; i < answer.length; i++) { //iterates through characters of string
-                //replaces underscore with if guess character is at the current index
-                if (answer[i] === guess) {
-                    incompleteWord[i] = answer[i];
-                }
-            }
-            //update 
-
-        }
-        else {
-            //display "incorrect guess" in user feedback element
-            //put guess in incorrect char array
-            incorrectChars = incorrectChars + guess;
-        }
-        
-        //lose condition
-        if(MAX_INCORRECT - incorrectChars.length < 1){
+        console.log("incorrectcharslength " + wrongChars.length);
+        if(remainingGuesses < 1){
             //display loss text and display text asking them if they want to play again in user feedback element
+            messageEL.textContent = "Woops! You lost! Press any key to play again";
+            incompleteWordEL.textContent = "Incomplete word: " + arrayToString(answer);
 
 
             //increment losses
@@ -101,9 +181,11 @@ document.onkeyup = function (event) {
             isPlaying = false;
         }
 
-        // win condition
-        if (incompleteWord === solution) {
+        /////////////////////YOU WON/////////////////////
+
+        if (incompleteWord.indexOf("_") === -1) {
             //display win text and display text asking them if they want to play again in user feedback element
+            messageEL.textContent = "Hooray! You win! Press any key to play again";
 
             wins++;
             ///update win display
